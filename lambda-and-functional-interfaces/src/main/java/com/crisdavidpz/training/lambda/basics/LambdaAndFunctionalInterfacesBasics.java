e6ab24cb4652bea40b9ee65c7296f953a97cbdb0
Cristian David Parra Zuluaga <cristian.parra.contacto@gmail.com>
2021-04-11 18:39:42 -0500

    Using Functional Interface to obtain some the value from a Lambda expression.0

--- lambda-and-functional-interfaces/src/main/java/com/crisdavidpz/training/lambda/basics/LambdaAndFunctionalInterfacesBasics.java
+++ lambda-and-functional-interfaces/src/main/java/com/crisdavidpz/training/lambda/basics/LambdaAndFunctionalInterfacesBasics.java
@@ -1,11 +1,21 @@
 package com.crisdavidpz.training.lambda.basics;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class LambdaAndFunctionalInterfacesBasics {
     public static void main(String[] args) {
-        Consumer<String> favoriteMotorcycle =
+        Consumer<String> fnFavoriteMotorcycle =
                 motorcycle -> System.out.println("My favorite motorcycle is " + motorcycle);
-        favoriteMotorcycle.accept("Honda XR 250");
+        fnFavoriteMotorcycle.accept("Honda XR 250");
+
+        Supplier<Integer> fnknowAge = () -> 34;
+        System.out.println("My age is " + fnknowAge.get() + " years.");
+
+        Function<Integer, String> fnAgeToDrive =
+                age -> age >= 18 ? "You can drive in Colombia!" : "Go in a bus!";
+        System.out.println(fnAgeToDrive.apply(14));
+        System.out.println(fnAgeToDrive.apply(34));
     }
 }
