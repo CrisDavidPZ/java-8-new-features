fb987de0aa43d3eb6d8fdacfbb6ea0857f648417
Cristian David Parra Zuluaga <cristian.parra.contacto@gmail.com>
2021-04-12 17:47:34 -0500

    Coding with Constructor method reference.

--- method-references/src/main/java/com/crisdavidpz/training/method/references/basics/MethodReferencesBasics.java
+++ method-references/src/main/java/com/crisdavidpz/training/method/references/basics/MethodReferencesBasics.java
@@ -1,6 +1,8 @@
 package com.crisdavidpz.training.method.references.basics;
 
 import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -10,15 +12,20 @@ public class MethodReferencesBasics {
         System.out.println();
 
         // Bounded instance method reference
-        Consumer<String> fnGreeter = System.out::println;
-        fnGreeter.accept("Hello from a bound method reference!");
+        Consumer<String> fnGreeterBoundInstance = System.out::println;
+        fnGreeterBoundInstance.accept("Hello from a bound method reference!");
 
         // Unbound instance method reference
-        BiConsumer<PrintStream, String> fn2Greeter = PrintStream::println;
-        fn2Greeter.accept(System.out, "Hello from an unbound instance method reference!");
+        BiConsumer<PrintStream, String> fnGreeterUnboundInstance = PrintStream::println;
+        fnGreeterUnboundInstance.accept(System.out, "Hello from an unbound instance method reference!");
 
         // Static method reference
-        Supplier<Thread> currentThread = Thread::currentThread;
-        System.out.println("The current thread is " + currentThread.get());
+        Supplier<Thread> fnCurrentThreadStaticMethod = Thread::currentThread;
+        System.out.println("The current thread is " + fnCurrentThreadStaticMethod.get());
+
+        // Contructor method reference
+        Supplier<Calendar> fnCalendarConstructorMethodReference = GregorianCalendar::new;
+        Calendar newCalendar = fnCalendarConstructorMethodReference.get();
+        System.out.println("The new calendar is " + newCalendar.getTime());
     }
 }
